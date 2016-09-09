f1f7b32c3a9a1e9865dd734a0cc93a2a446d2a0e
Gustavo <gus.juarezoseguera@gmail.com>
2016-09-09 13:38:03 -0500

    Version_1.1

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python
 
 # import modules used here -- sys is a very standard one
-import sys
+import
 
 # Gather our code in a main() function
 def main():
   print ('Hello there')
+  print ("Ya lo modifique")
   # Command line args are in sys.argv[1], sys.argv[2] ..
   # sys.argv[0] is the script name itself and can be ignored
 
